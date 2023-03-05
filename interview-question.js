4dc5160e0e6a12a51834a3c0ee35b15ebc5865ad
sajibsaifullah <dusajibphy@gmail.com>
2023-03-05 18:47:11 +0600

    added

---
+++ interview-question.js
@@ -0,0 +1,11 @@
+/* 
+1. Tell me some thing about JS engine v8 internal mechanism.
+2. What is event loop in JavaScript?
+3. If javascript single threaded, how does it handle asynchronous call?
+4. Difference between setTimeout & setInterval.
+5. Can you explain the difference between an async func & a regular function in javascript?
+6. What's the error handling strategy for promises that were rejected while awaiting?
+7. What are the state of a promise?
+8. Can you explain what the .then() method does?
+
+*/
